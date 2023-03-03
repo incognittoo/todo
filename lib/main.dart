cc617df5201168ed2c92809a7c23f36b6df8fd92
Yusuf <usufbogatyrev@gmail.com>
2023-03-03 21:59:52 +0300

    Редактирован gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todo/pages/home.dart';
 import 'package:todo/pages/note_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-      theme: ThemeData(
-          colorScheme: ColorScheme.dark().copyWith(primary: Colors.blue)),
-      initialRoute: '/',
-      routes: {'/': (context) => Home(), '/note': (context) => NoteScreen()},
-    ));
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.dark().copyWith(primary: Colors.blue)),
+        initialRoute: '/',
+        routes: {'/': (context) => Home(), '/note': (context) => NoteScreen()},
+      ),
+    );
